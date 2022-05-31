7ee5d50705e746c80a7f47241b7bbb424385260b
Rahilaam <khadeejathrahilaam@gmail.com>
2022-05-31 18:44:25 +0200

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+export default function Navbar() {
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div></div>
+    </div>
+  );
+}
