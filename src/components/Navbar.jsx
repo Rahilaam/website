b26ea0e6e9da95cff7bc6a5b7ff51eda4d4cc944
Rahilaam <khadeejathrahilaam@gmail.com>
2022-05-31 23:01:46 +0200

    navbar second day

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function Navbar() {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-      <div></div>
+      <div>
+        <img
+          src={logo}
+          alt="my logo here"
+          style={{ width: "50px", height: "50px" }}
+        />
+      </div>
+      {/*menu*/}
+      <div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      {/*hamburger*/}
+      <div className="hidden">
+        <FaBars />
+      </div>
+      {/*mobile menu*/}
+      <ul className="hidden">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      {/*social icons*/}
+      <div className="hidden"></div>
     </div>
   );
 }
